fix reward calculation

agents/dqn/bagent.py
@@ -66,6 +66,68 @@ def sample(buffer, size):
         result.append(buffer[i])
     return result
 
+def fn_constantbehavioralcontrol(ctime, owner, min, max, initial_value, current_value, target_value):
+    dif = current_value - initial_value
+    delta = 0.1 if dif > 0 else -0.1
+    delta = delta/dif
+    return current_value - delta
+
+def fn_linearbehavioralvalue(ctime, owner, min, max, initial_value, current_value):
+    c = min + 0.5 * (max - min)
+    d = abs(current_value - c)
+    if d > 0:
+        return 2.0 * (max-min)/d - 1.0
+    else:
+        return 2.0
+
+def baredom_control(ctime, owner, min, max, initial_value, current_value, target_value):
+    owner.delta_time = ctime - owner.start_time
+    if owner.delta_time > 100:
+        owner.action_count = [0]*7
+        owner.start_time = ctime
+        owner.baredom_freq = float(owner.action_count[0])/owner.delta_time
+    else:
+        owner.action_count[owner.last_action] += 1
+    return owner.baredom_freq
+
+class BehavioralRange:
+    def __init__(self, name,  owner, initial_value, min=0.0, max=1.0, target_value=2.0, fn_value=fn_linearbehavioralvalue, fn_control=fn_constantbehavioralcontrol):
+        self.name = name
+        self.owner = owner
+        self.min = min
+        self.max = max
+        self.target_value = target_value
+        self.initial_value = initial_value
+        self.current_value = initial_value
+        self.fn_value = fn_value
+        self.fn_control = fn_control
+
+    def update(self, ctime):
+        self.current_value = self.fn_control(ctime, self.owner, self.min, self.max, self.initial_value, self.current_value, self.target_value)
+        return self.fn_value(ctime, self.owner, self.min, self.max, self.initial_value, self.current_value)
+
+
+class BehavioralEngine:
+    def __init__(self, behaviors =  []):
+        self.behaviors = behaviors
+        self.sum_of_values = 0.0
+    
+    def update(self, ctime):
+        values = {}
+        self.sum_of_values = 0.0
+        for b in self.behaviors:
+            v = b.update(ctime)
+            values[b.name] = v
+            self.sum_of_values += v
+        return values
+
+    def avg_value(self):
+        if len(self.behaviors) > 0:
+            return self.sum_of_values/len(self.behaviors)
+        else:
+            return 0.0  
+
+
 class DQNAgent:
     def __init__(self, state_size, action_size):
 
@@ -86,15 +148,18 @@ class DQNAgent:
         self.psize = 0
         self.nsize = 0
         self.ntsize = 0
-        self.skip_frames = 10
+        self.skip_frames = 4
         self.step = 0
         self.loss = 0.0
         self.count_loss = 1
         self.global_step = 0
-        self.contextual_actions = [0, 1, 2, 3, 4, 5, 6, 7]
+        self.contextual_actions = [0, 1, 2, 3, 4, 5, 6]
         self.epoch = 0
         self.mask_actions = np.ones(self.action_size).reshape(1, self.action_size)
         self.replay_is_running = False
+
+        self.baredom_freq = 0.0
+
         self.graph = tf.get_default_graph()
         self.session = keras.backend.get_session()
         self.model = self._build_model()
@@ -110,28 +175,32 @@ class DQNAgent:
         if is_new_epoch:
             self.epoch += 1
         self.step = 0
+        self.last_action = None
+        self.action_count = [0] * 7
+        self.delta_time = 0
+        self.start_time = 0
+        self.baredom = BehavioralRange("baredom", self, 0.0, 0.2, 0.5, fn_control=baredom_control)
 
     def _build_model(self):
-        ATARI_SHAPE = (self.state_size[0], self.state_size[1], self.skip_frames)  # input image size to model
+        ATARI_SHAPE = (self.skip_frames, self.state_size[0], self.state_size[1])  # input image size to model
         ACTION_SIZE = self.action_size
         # With the functional API we need to define the inputs.
         frames_input = layers.Input(ATARI_SHAPE, name='frames')
         actions_input = layers.Input((ACTION_SIZE,), name='action_mask')
-        #size = self.state_size[0] * self.state_size[1] * self.skip_frames
+        size = self.state_size[0] * self.state_size[1] * self.skip_frames
         normalize = layers.Lambda( lambda x : x/6.0) (frames_input)
-        reshape = layers.Flatten()(normalize)
-        #lstm_layer = layers.recurrent.LSTM(64, return_sequences=True)(reshape) 
-
-        hidden = layers.Dense(128)(reshape)
-        hidden2 = layers.Dense(128)(hidden)
-        output = layers.Dense(ACTION_SIZE)(hidden2)
+        flattened = layers.Flatten()(normalize)
+        reshape = layers.Reshape( (1, size) )(flattened)
+        lstm_layer = layers.recurrent.LSTM(32, return_sequences=True)(reshape) 
+        hidden = layers.Dense(32)(lstm_layer)
+        output = layers.Dense(ACTION_SIZE)(hidden)
 
         filtered_output = layers.Multiply(name='QValue')([output, actions_input])
 
         model = Model(inputs=[frames_input, actions_input], outputs=filtered_output)
         model.summary()
-        optimizer = RMSprop(lr=self.learning_rate, rho=0.95, epsilon=0.01)
-        
+        #optimizer = RMSprop(lr=self.learning_rate, rho=0.95, epsilon=0.01)
+        optimizer = Adam(lr=self.learning_rate)
         model.compile(optimizer, loss=huber_loss)
         return model
 
@@ -187,16 +256,15 @@ class DQNAgent:
                 self.epsilon = self.epsilon_min
 
     def act(self, state, is_randomic = False):
-        action = 0
         p = np.random.rand()
         if is_randomic or p <= self.epsilon:
-            self.update_internal(is_randomic)
-            return np.random.choice(np.arange(0, self.action_size))
+            self.last_action = np.random.choice(np.arange(0, self.action_size))
         else:
             act_values = self.model.predict([state, self.mask_actions])
-            action = np.argmax(act_values[0])
-            self.update_internal(is_randomic)
-            return action
+            self.last_action = np.argmax(act_values[0])
+        self.update_internal(is_randomic)
+        #self.baredom_value = self.baredom.update(self.step)
+        return self.last_action
 
 
     def replay(self, batch_size, postask=None):
@@ -214,9 +282,9 @@ class DQNAgent:
         batch_size = len(minibatch)
 
         states = np.zeros(
-            (batch_size, self.state_size[0], self.state_size[1], self.skip_frames))
+            (batch_size, self.skip_frames, self.state_size[0], self.state_size[1]))
         next_states = np.zeros(
-            (batch_size, self.state_size[0], self.state_size[1], self.skip_frames))
+            (batch_size, self.skip_frames, self.state_size[0], self.state_size[1]))
         actions = []
         rewards = []
         dones = []

agents/dqn/learning.py
@@ -13,7 +13,7 @@ import MazeWorld
 
 agent = Agent( (10, 10), 7)
 agent.front2back()
-agent.epsilon_decay = ((agent.epsilon - agent.epsilon_min)/500000)
+agent.epsilon_decay = ((agent.epsilon - agent.epsilon_min)/1000000)
 
 def pre_processing(observe):
     observe = np.array(observe)
@@ -23,21 +23,13 @@ def pre_processing(observe):
 
 LOSS = 0.0
 
-def back2front(agent, loss):
-    global LOSS
-    agent.loss += loss
-    agent.count_loss += 1
-    LOSS += loss
-    agent.back2front()
-
-
 RENDER = False
 REFRESH_MODEL_NUM = 10000
-N_RANDOM_STEPS = 5000
+N_RANDOM_STEPS = 50000
 MAX_EPSODES = 100000000
 NO_OP_STEPS = 30
-FRAME_SKIP = 10
-NOOP_ACTION = 6
+FRAME_SKIP = 4
+NOOP_ACTION = [0,1,6]
 
 env = gym.make('MazeWorld-v0')
 
@@ -53,34 +45,39 @@ for i in range(MAX_EPSODES):
     action = 0
     next_state = None
 
-    for _ in range(NO_OP_STEPS):
-        frame = env.step(NOOP_ACTION, FRAME_SKIP)[0]
+    for _ in range(random.randint(0, NO_OP_STEPS)) :
+        action = random.sample(NOOP_ACTION,1)[0]
+        frame = env.step(action, FRAME_SKIP)[0]
 
     if not env.useRaycasting:
         frame = pre_processing(frame)
 
     stack_frame = tuple([frame]*agent.skip_frames)
     initial_state = np.stack(stack_frame, axis=2)
-    initial_state = np.reshape([initial_state], (1, env.vresolution, env.hresolution, agent.skip_frames))
+    initial_state = np.reshape([initial_state], (1, agent.skip_frames, env.vresolution, env.hresolution))
     score = 0
     dead = False
-    freq = np.zeros(agent.action_size)
     while not is_done:
         dead = False
         if agent.global_step >= N_RANDOM_STEPS:
             action = agent.act(initial_state)
         else:
             action = agent.act(initial_state, True)
-        freq[action] += 1
+
         frame, reward, is_done, info = env.step(agent.contextual_actions[action], FRAME_SKIP)
+
+        #reward += agent.baredom_value
 
         next_frame = frame
         if not env.useRaycasting:
             next_frame = pre_processing(next_frame)
-        next_state = np.reshape([next_frame], (1, env.vresolution, env.hresolution, 1))
-        next_state = np.append(next_state, initial_state[:, :, :, :(FRAME_SKIP-1)], axis=3)
-        score += reward
+        
+        next_state = np.reshape([next_frame], (1, 1, env.vresolution, env.hresolution))
+        next_state = np.append(next_state, initial_state[:, :(FRAME_SKIP-1), :, :], axis=1)
+        
         reward = np.clip(reward, -1.0, 1.0)
+
+        score += reward
     
         end_eps = dead or is_done
         

gym/MazeWorld/envs/env.py
@@ -53,7 +53,7 @@ class Environment(gym.Env):
         self.observation_space = spaces
         self.observation_space = spaces.Box(low=0, high=self.maxValue, shape=(self.vresolution, self.hresolution, 1), dtype=np.uint8)
         self.last_frame = None
-        
+
         path = os.environ['MW_PATH']
         
         cmd = None
@@ -122,6 +122,8 @@ class Environment(gym.Env):
         return self._agent_perception()
 
     def reset(self):
+        self.initial_energy = 15
+        self.prev_perception = None
         actions.restart(self.net)
         sleep(0.2)
         _, frame = self._get_one_step(0)
@@ -133,27 +135,38 @@ class Environment(gym.Env):
     def render(self, mode='human', close=False):
         return self.last_frame
 
-    def _prepare_data(self, info, frame):
+    def _prepare_data(self, info, frame, action):
         if not self.useRaycasting:
             frame = Image.open(io.BytesIO(frame))
+
+        pinfos = None
+        reward = 0
+
+        if self.prev_perception:
+            _, _, _, pinfos = self.prev_perception
+            wasPickUpNear = pinfos['isPickUpNear']
+            pickUpValue = pinfos['nearPickUpValue']
+            if wasPickUpNear and ACTION_MEANING[action] == "PICKUP":
+                reward += pickUpValue
 
         lives = info[0]
         self.nlives = lives
         energy = info[1]
-        score = info[2]
+        #score = info[2]
         done = info[-3]
-        isPickUpNear = True if info[-2] == 0 else False
+        isPickUpNear = True if info[-2] == 1 else False
         nearPickUpValue = info[-1]
 
+        '''
         diff = energy - self.initial_energy + score
         self.initial_energy = energy
-
-        if diff < -1 or energy <= 0:
+        if diff < -1:
             reward = -1
         elif diff > 1:
             reward = 1
         else:
             reward = 0
+        '''
 
         infos = {'lives': lives, 'energy': energy, 'isPickUpNear': isPickUpNear, 'nearPickUpValue': nearPickUpValue}
         self.last_frame = frame
@@ -166,13 +179,14 @@ class Environment(gym.Env):
         actions.resume(self.net)
         info = None
         frame = None
-        action = self.action_map[action]
+        maction = self.action_map[action]
         if frame_skip > 0:
             for _ in range(frame_skip-1):
-                ACTIONS[action](self.net, cmdtype="action ignore frame")
-        info, frame = self._get_one_step(action)
+                ACTIONS[maction](self.net, cmdtype="action ignore frame")
+        info, frame = self._get_one_step(maction)
+        self.prev_perception = self._prepare_data(info, frame, action)
         actions.pause(self.net)
-        return self._prepare_data(info, frame)
+        return self.prev_perception
 
     def __del__(self):
         self.net.close()